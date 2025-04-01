623edc20742f915f4dcb3dd86025d9e711b119b7
MarcosGTS <marcosgtsodre@gmail.com>
2025-04-01 19:09:17 -0300

    Create modal

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+const DEFAULT_IMAGE = "/imgs/loading.gif";
+const DEFAULT_TEXT  = "Fazendo requisição ...";
+
+const modal = document.querySelector('.modal__background');
+const confimBtn = modal.querySelector('.form__button__confirm');
+
+modal.show = function () {
+    this.classList.remove("hidde");
+}
+
+modal.hidde = function () {
+    this.classList.add("hidde");
+}
+
+modal.setImage = function (src) {
+    const img = modal.querySelector("img");
+    img.src = src;
+}
+
+modal.setText = function (message) {
+    const text = modal.querySelector("p");
+    text.innerText = message;
+}
+
+confimBtn.addEventListener("click", () => {
+    const modal = confimBtn.closest(".modal__background");
+    modal.setText(DEFAULT_TEXT);
+    modal.setImage(DEFAULT_IMAGE);
+    modal.hidde();
+})
+
+
+export {modal};
